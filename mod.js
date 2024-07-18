b4abfca8ab7737955f26136457d95c63a1d03aea
Rodion Liskun <rodion.liskun@gmail.com>
2024-07-19 00:44:23 +0300

    Fix: mod.js #4

--- mod.js
+++ mod.js
@@ -504,7 +504,7 @@ Date.now||(Date.now=function(){return(new Date.getTime())}),function(){"use stri
           Pub.network.timeout(15000);
           Pub.network.silent(API + 'device/auth', function(json) {
             if (!json.success) window.location.reload();
-            var auth = true;
+            var auth = json.auth;
             logged = auth;
 
             console.log('Modss', 'auth', auth);
